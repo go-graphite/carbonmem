go-graphite/carbonmem

commit ff44b284c8d6fde2edffedca3d6201cd7fd4dc9a

Keep a free list of metric IDs
This should reduce infinite growth due to holes in the metric list.

Fixes #7

mem.go
@@ -444,6 +444,8 @@ type lookup struct {
 	active map[MetricID]int
 	prefix *radix.Tree
 
+	free []MetricID
+
 	pathidx trigram.Index
 	paths   []string
 
@@ -479,8 +481,12 @@ func (l *lookup) FindOrAdd(key string) MetricID {
 		return id
 	}
 
-	id = MetricID(l.count)
-	l.count++
+	if len(l.free) == 0 {
+		id = MetricID(l.count)
+		l.count++
+	} else {
+		id, l.free = l.free[0], l.free[1:]
+	}
 
 	l.keys[key] = id
 	l.revs = append(l.revs, key)
@@ -525,6 +531,7 @@ func (l *lookup) DelRef(id MetricID) {
 			l.pathidx.Delete(l.paths[id], trigram.DocID(id))
 		}
 		l.paths[id] = ""
+		l.free = append(l.free, id)
 	}
 }
 

commit 4faab1d417fa215f6469aaacd4616ae7ac230066

Fix trigram indexing bug
This fixes a bug in trigram indexing that was introduced in
ff44b284c8d6fde2edffedca3d6201cd7fd4dc9a .

Once we started using the freelist, document IDs were no longer being
inserted in sorted order, and the document IDs wouldn't match the path
numbers.  This would cause globbing queries to fail as the query code
assumes all the posting lists are sorted, and even if the IDs did come
back they might not back the entries in l.paths[].

This patch addresses this by only using the free list only if
we're not using the trigram index.

mem.go
@@ -481,21 +481,24 @@ func (l *lookup) FindOrAdd(key string) MetricID {
 		return id
 	}
 
-	if len(l.free) == 0 {
+	useTrigramIndex := l.pathidx != nil && (l.trigramCutoff == 0 || len(l.keys) < l.trigramCutoff)
+
+	path := strings.Replace(key, ".", "/", -1) + ".wsp"
+
+	if len(l.free) == 0 || useTrigramIndex {
 		id = MetricID(l.count)
 		l.count++
 		l.revs = append(l.revs, key)
+		l.paths = append(l.paths, path)
 	} else {
 		id, l.free = l.free[0], l.free[1:]
 		l.revs[id] = key
+		l.paths[id] = path
 	}
 
 	l.keys[key] = id
 
-	path := strings.Replace(key, ".", "/", -1) + ".wsp"
-	l.paths = append(l.paths, path)
-
-	if l.pathidx != nil && (l.trigramCutoff == 0 || len(l.keys) < l.trigramCutoff) {
+	if useTrigramIndex {
 		l.pathidx.Insert(path, trigram.DocID(id))
 	} else {
 		l.pathidx = nil

mem_test.go
@@ -33,7 +33,7 @@ func TestMemStore(t *testing.T) {
 		{"bar", 305, 33, 120, 300, []float64{22, 33}},
 	}
 
-	w := NewWhisper(100, 10, 120)
+	w := NewWhisper(100, 10, 120, TrigramCutoff(1))
 
 	var fooid MetricID = math.MaxUint32
 